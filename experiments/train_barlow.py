c152dec8c4f2fb4ca8041b0d852c9327bf0b1cbb
CorentinAmbroise <corentin.ambroise9132@gmail.com>
2023-05-30 17:51:56 +0200

    using interval for augmentations in train_barlow

--- experiments/train_barlow.py
+++ experiments/train_barlow.py
@@ -19,7 +19,7 @@ import torch
 from torch import nn, optim
 from torchvision import transforms
 from surfify.models import SphericalHemiFusionEncoder
-from surfify.augmentation import SurfCutOut, SurfBlur, SurfNoise
+from surfify.augmentation import SurfCutOut, SurfBlur, SurfNoise, interval
 from surfify.utils import setup_logging, icosahedron, downsample_data, downsample, min_depth_to_get_n_neighbors
 from brainboard import Board
 
@@ -359,7 +359,7 @@ for modality in modalities:
         ico = backbone.ico[args.ico_order]
         trf = SurfBlur(
             ico.vertices, ico.triangles, None,
-            sigma=(0.1, 2),
+            sigma=interval((0.1, 2), float),
             cachedir=os.path.join(args.outdir, "cached_ico_infos"))
         if args.algo == "barlow":
             transformer.register(trf, pipeline="hard")
@@ -367,7 +367,7 @@ for modality in modalities:
         else:
             transformer.register(trf, probability=0.5)
     if args.noise:
-        trf = SurfNoise(sigma=(0.1, 2))
+        trf = SurfNoise, interval(sigma=interval((0.1, 2), float))
         if args.algo == "barlow":
             transformer.register(trf, pipeline="hard")
             transformer.register(trf, probability=0.1, pipeline="soft")
@@ -379,7 +379,7 @@ for modality in modalities:
         patch_size = min_depth_to_get_n_neighbors(np.ceil(len(ico.vertices) / 4))
         trf = SurfCutOut(
             ico.vertices, ico.triangles, None,
-            patch_size=patch_size,
+            patch_size=interval((1, patch_size), int),
             cachedir=os.path.join(args.outdir, "cached_ico_infos"))
         # print(time.time() - t)
         if args.algo == "barlow":
